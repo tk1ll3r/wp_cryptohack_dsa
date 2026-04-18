795e506e216b3ba15416199f00a89ed22bf6b95c
baht21 <25520671@gm.uit.edu.vn>
2026-04-18 21:05:04 +0700

    Create Base64.py

---
+++ general/Base64/Base64.py
@@ -0,0 +1,7 @@
+import base64
+
+hex_string = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
+raw_bytes = bytes.fromhex(hex_string)
+encoded = base64.b64encode(raw_bytes)
+
+print(encoded.decode())
